94ee00726bf5c794f1e6d1e79bec6b69cec363b0
manasa kompalli <manasaravali23@gmail.com>
2020-07-06 01:21:20 +0530

    refs #week3 service discovery and cloud config

--- src/main/java/com/jpop/bookservice/BookServiceApplication.java
+++ src/main/java/com/jpop/bookservice/BookServiceApplication.java
@@ -1,9 +1,12 @@
 package com.jpop.bookservice;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BookServiceApplication {
 
 	public static void main(String[] args) {
